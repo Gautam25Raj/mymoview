6e85f4232e225ac215ed314bfe98d0833964d28d
Gautam Raj <ashragautam25@gmail.com>
2023-09-03 01:59:08 +0530

    Update

--- app/api/config/route.js
+++ app/api/config/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request) {
+export async function GET(request, { params }) {
   const bearerToken = process.env.BEARER_TOKEN;
   const apiKey = process.env.API_KEY;
 
